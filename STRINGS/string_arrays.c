5163328d1eba7672485c44ad26bfbdf5c9f8a665
gurpartap <gurpartapsingh007@gmail.com>
2014-10-17 18:46:46 +0530

    string_arrays.c

---
+++ STRINGS/string_arrays.c
@@ -0,0 +1,16 @@
+//reference page 362
+#include <stdio.h>
+#define LIM 5
+int main(){
+const char *mytal[LIM] = {"Adding numbers swiftly",
+"Multiplying accurately", "Stashing data",
+"Following instructions to the letter",
+"Understanding the C language"};
+printf("string is %s\n",mytal[0]);
+printf("string is %s\n",mytal[1]);
+printf("string is %s\n",mytal[2]);
+printf("string is %s\n",mytal[3]);
+printf("string is %s\n",mytal[4]);
+
+   return 0;
+}
