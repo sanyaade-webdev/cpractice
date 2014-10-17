2d4906a78d8016bbbd5ecd082a8a8b3972f328c5
gurpartap <gurpartapsingh007@gmail.com>
2014-10-17 19:08:42 +0530

    deleted

--- STRINGS/string_arrays.c
+++ STRINGS/string_arrays.c
@@ -1,16 +1,16 @@
-//reference page 362
 #include <stdio.h>
-#define LIM 5
 int main(){
-const char *mytal[LIM] = {"Adding numbers swiftly",
-"Multiplying accurately", "Stashing data",
-"Following instructions to the letter",
-"Understanding the C language"};
-printf("string is %s\n",mytal[0]);
-printf("string is %s\n",mytal[1]);
-printf("string is %s\n",mytal[2]);
-printf("string is %s\n",mytal[3]);
-printf("string is %s\n",mytal[4]);
-
+   char c[1000],ch;
+   int i,count=0;
+   printf("Enter a string: ");
+   gets(c);
+   printf("Enter a characeter to find frequency: ");
+   scanf("%c",&ch);
+   for(i=0;c[i]!='\0';++i)
+   {
+	  if(ch==c[i])
+		 ++count;
+   }
+   printf("Frequency of %c = %d", ch, count);
    return 0;
 }
