3b4daa2e8f37b35a4c6b1093c7127e79f993a688
gurpartap <gurpartapsingh007@gmail.com>
2014-10-18 15:43:06 +0530

    added scanf_loop & string_cmp

--- STRINGS/string_input.c
+++ STRINGS/string_input.c
@@ -11,9 +11,9 @@ gets(name);//it get spaces and return address of pointer which is passed to it i
 fgets(name1,150,stdin);//it get spaces and reads upto maximum size
 printf("string =%s \n",name);
 printf("string =%s \n",name1);
-scanf("%5s",name2);
-scanf("%6s",name3);
-printf("string =%s \n",name2);
-printf("string =%s \n",name3);
+scanf("%5s",name2);//it only scans upto 5
+scanf("%s",name3);//scanf terminate on 2 conditions : enter and space. if we enter hhhhh gggggg then it takes hhhhh and it points at starting of gggggg.next scanf starts scanning from position which it lefts.so it reads from gggggg wothout executing next scanf
+printf("string =%s \n",name2);//outputs hhhhh
+printf("string =%s \n",name3);//outputs gggggg
 return 0;
 }
--- basic/float_type.c
+++ basic/float_type.c
@@ -10,6 +10,9 @@ float max=2.0e4;
 float add=2.0e4+1;
 max=add-2.0e4;
 printf("value of max is %f\n",max);//reason for odd answer when we use 2.0e20 because float only considers 6 decimal places.But in our case these are 20.So,we get undefined answer.
-
+printf("size of %d\n",sizeof(long ));
+printf("size of %d\n",sizeof(long long ));
+printf("size of %d\n",sizeof( double));
+printf("size of %d\n",sizeof(long double));
 return 0;
 }
