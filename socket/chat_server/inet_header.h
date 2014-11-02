5008cc4541ee2254750b0f41b45664d50ccc5450
gurpartap <gurpartapsingh007@gmail.com>
2014-11-02 15:47:13 +0530

    added socket folder on pc

---
+++ socket/chat_server/inet_header.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<string.h>
+#include<pthread.h>
+#define ID_LEN 10
