ef5edd666fb0a1ebf7952afc2ed0fefef8815c31
gurpartap <gurpartapsingh007@gmail.com>
2014-11-09 16:14:41 +0530

    added pointers questions 1 and 2

---
+++ sanfoundry/pointer_func_argument1_7##.c
@@ -0,0 +1,29 @@
+        #include <stdio.h>
+
+        int main()
+
+        {
+
+            int i = 10;
+
+            int *p = &i;
+
+            foo(&p);
+
+            printf("%d ", *p);
+
+            printf("%d ", *p);
+
+        }
+
+        void foo(int **const p)
+
+        {
+
+            int j = 11;
+
+            *p = &j;
+
+            printf("%d ", **p);
+
+        }
